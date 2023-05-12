4f4dd21a2b86c03acc72d207d2be76a5630d8fc8
Navbharat77 <111179889+Navbharat77@users.noreply.github.com>
2023-05-12 09:58:39 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-#_Change_Working_Directory
-cd /home/ec2-user/server
+# navigate to app folder
+cd /app
 
-#_Update_&_Set_Node_Version
+# install node and npm
 curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -
-
-#_Download_Node_&NPM
 yum -y install nodejs npm
-
-#_Download_PM2
-npm install pm2@latest -g
